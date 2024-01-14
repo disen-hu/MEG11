41c1b63290c907e739ed770e7fbd6d54520b280f
disen-hu <1515092118@qq.com>
2024-01-14 11:33:39 +0100

    amende

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 To start with the code you should first look at the run experiment file. 
 
-
+run with # python run_exeriment.py --config config_***.yaml
 
 "# MEG" 
 "# MEG11" 
--- run_experiment.py
+++ run_experiment.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
             run_name = input()
             run_name = 'intra_'+run_name
             config['run_name'] = run_name
-            wandb.init(project='MEG', config=config, name = run_name)
+            wandb.init(entity='deceminc',project='pr', config=config, name = run_name)
 
         # Extract test and training data from the dataset
         X_train, y_train = data['intra']['X_train'], data['intra']['y_train']
@@ -117,7 +117,7 @@ if __name__ == '__main__':
                 run_name = input()
             run_name = 'cross_'+run_name
             config['run_name'] = run_name
-            wandb.init(project='MEG', config=config, name = run_name)
+            wandb.init(entity='deceminc',project='pr', config=config, name = run_name)
         
         # Extract test and training data from the dataset
         X_train, y_train = data['cross']['X_train'], data['cross']['y_train']
